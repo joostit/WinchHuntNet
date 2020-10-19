c7ba42f85042fe866809a152fbbb7f1629e24c2c
Joost Haverkort <dev@joostit.com>
2020-10-19 12:46:21 +0200

    First code commit. Empty Console app

---
+++ WinchHuntNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WinchHuntNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
